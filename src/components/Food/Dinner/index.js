2e94722c9d32423322284d168ed34c1155bc80a5
Kelly Ou <kelleo0703@gmail.com>
2022-12-29 03:25:07 -0500

    Completed fetchRandom function.

--- src/components/Food/Dinner/index.js
+++ src/components/Food/Dinner/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import "./index.css";
+import React, { useState, useEffect } from 'react';
+import { collection, addDoc, getDocs, FieldPath } from "firebase/firestore";
+import { db } from '../../../firebase';
 
 const Dinner = () => {
+  var food = "";
+  const [ran, setRan] = useState([]);
+
+  function randomNum(max, min){
+    const r = Math.random()*(max-min) + min
+    return Math.floor(r)
+}
+  const fetchRandom = async () => {
+    await getDocs(collection(db, "dinner"))
+            .then((querySnapshot) => {
+                const newData = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                console.log(newData);
+                const num = newData.length;
+                const ranNum = randomNum(0, num);
+                console.log(num);
+                console.log(newData.at(ranNum));
+                console.log(newData.at(ranNum).name);
+                food = newData.at(ranNum).name;
+                console.log(food);
+                setRan(food);
+                console.log(ran);
+            })
+   }
+
+   useEffect(() => {
+    fetchRandom();
+}, [])
+   
   return (
-    <div>
-      
-    </div>
+    <div>{ran}</div>
   )
+  
 }
 
 export default Dinner
