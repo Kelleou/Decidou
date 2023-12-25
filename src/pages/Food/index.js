0d876821c4cfa0436bf1fbbeb60162186b9b57bd
oujiayi <kelly.ou@mail.utoronto.ca>
2023-12-25 18:54:01 -0500

    style fixes

--- src/pages/Food/index.js
+++ src/pages/Food/index.js
@@ -8,25 +8,27 @@ export default function Food() {
     <div className="food-page container md:container md:mx-auto text-center">
       <h1> Hello! How is your day? </h1>
       <h2>Having a hard time deciding what to eat again?</h2>
-      <div className="grid grid-cols-2 m-10 gap-0">
-        <span className="mr-5">
-          <Link to="/breakfast" className="btns">
-            Breakfast
-          </Link>
-          <Link to="/lunch" className="btns">
-            Lunch
-          </Link>
-          <Link to="/dinner" className="btns">
-            Dinner
-          </Link>
-          <Link to="/snack" className="btns">
-            Snack
-          </Link>
-          <Link to="/menu" className="btns">
-            See Menu
-          </Link>
-        </span>
-        <img src={Decidou} alt="decidou" />
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-2 m-10 gap-0">
+          <span className="mr-5">
+            <Link to="/breakfast" className="btns">
+              Breakfast
+            </Link>
+            <Link to="/lunch" className="btns">
+              Lunch
+            </Link>
+            <Link to="/dinner" className="btns">
+              Dinner
+            </Link>
+            <Link to="/snack" className="btns">
+              Snack
+            </Link>
+            <Link to="/menu" className="btns">
+              See Menu
+            </Link>
+          </span>
+          <img src={Decidou} alt="decidou" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,40 @@
 import { Link } from "react-router-dom";
 import "./index.css";
 import Decidou from "../../assets/Images/decidou_hi.PNG";
-export default function Home(){
+export default function Home() {
   return (
     <div className="container md:container md:mx-auto text-center">
       <h1> Hello! I am Decidou! </h1>
       <h2>I am here to help you make a decision!</h2>
-      <div className="grid grid-cols-2 m-10 gap-0">
-        <span>
-        <Link to="/task" className="btns">
-            Help me schedule tasks!
-          </Link>
-          <Link to="/food" className="btns">
-            Help me decide what to eat!
-          </Link>
-          <Link to="/answer" className="btns">
-            I am lost!
-          </Link>
-          <Link to="/coin" className="btns">
-            Flip a coin!
-          </Link>
-          <Link to="/about" className="btns">
-            More about Dicidou
-          </Link>
-        </span>
-        <span> <img src={Decidou} alt="decidou" className="hi object-contain h-full w-full" /></span>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-2 m-10 gap-5">
+          <span>
+            <Link to="/task" className="btns">
+              Help me schedule tasks!
+            </Link>
+            <Link to="/food" className="btns">
+              Help me decide what to eat!
+            </Link>
+            <Link to="/answer" className="btns">
+              I am lost!
+            </Link>
+            <Link to="/coin" className="btns">
+              Flip a coin!
+            </Link>
+            <Link to="/about" className="btns">
+              More about Dicidou
+            </Link>
+          </span>
+          <span>
+            {" "}
+            <img
+              src={Decidou}
+              alt="decidou"
+              className="hi object-contain h-full w-full"
+            />
+          </span>
+        </div>
       </div>
     </div>
   );
-};
+}
